9166db54ecc68073bf74f649f67a035d11c58fb2
PSNCroned <3lliotn98@gmail.com>
2017-12-18 20:47:55 -0700

    fixed bug

--- game.js
+++ game.js
@@ -183,12 +183,12 @@ class Game {
 				for (let i = 0; i < this.roles[color][role]; i++) {
 					switch (color) {
 						case "red":
-							player = utils.randArrVal(redTemp);
+							let player = utils.randArrVal(redTemp);
 							this.players[player].role = role;
 							redTemp.splice(redTemp.indexOf(player), 1);
 							break;
 						case "blue":
-							player = utils.randArrVal(blueTemp);
+							let player = utils.randArrVal(blueTemp);
 							this.players[player].role = role;
 							blueTemp.splice(blueTemp.indexOf(player), 1);
 							break;
